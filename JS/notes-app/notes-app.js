8c955078e7836bbdf3a59bc5ee45624eb8f43248
David Morley <davidkmorley@gmail.com>
2018-05-23 18:02:56 +0100

    added filters to both apps

--- JS/notes-app/notes-app.js
+++ JS/notes-app/notes-app.js
@@ -9,6 +9,25 @@ const notes = [{
     body: 'generally whatever'
 }]
 
+const filters = {
+    searchText: ''
+}
+
+const renderNotes = (notes, filters) => {
+    const filteredNotes = notes.filter((note) => {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+    filteredNotes.forEach(note =>{
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+renderNotes(notes, filters)
+
 document.querySelector('#create-note').addEventListener('click', (e) =>{
  e.target.textContent = 'The button was clicked'
  
@@ -22,5 +41,6 @@ document.querySelector('#delete-all').addEventListener('click', (e) =>{
    })
 
 document.querySelector('#search').addEventListener('input', (e) => {
-   console.log(e.target.value)    
+   filters.searchText = e.target.value  
+   renderNotes(notes, filters)  
 })
--- JS/todo-app/main.js
+++ JS/todo-app/main.js
@@ -14,28 +14,36 @@ const todos = [{
        task:  'go to Le Wagon?',
        completed: false
    }]
-const todoButton = document.getElementById('to-do')
-var counter = 0;
-todos.forEach(todo => {
-    if (!todo.completed){
-        counter += 1;
-        const newToDo = document.createElement('p')
-        newToDo.textContent = `${todo.task}`
-        todoButton.insertAdjacentElement('afterend', newToDo)
+
+   const textFilter = {
+       param: ''
+   }
+   
+   const renderToDos = (list, filter) => {
+    //    clear div where todos are listed by filter
+       document.querySelector("#todos").innerHTML = '';
+    //    filter todos against search parameter
+       const filteredTodos = list.filter(todo => todo.task.toLowerCase().includes(filter.param.toLowerCase()))
+    //    filter these search-filtered todos by whether they are completed or not
+       const incompleteTodos = filteredTodos.filter(todo => !todo.completed )
+       const summary = document.createElement('h2')
+       summary.textContent = `You have ${incompleteTodos.length} tasks left:`
+       document.querySelector('#todos').appendChild(summary)
+    // Add list of todos to div 
+       incompleteTodos.forEach(todo => {
+            const p = document.createElement('p')
+            p.textContent = todo.task
+            document.querySelector('#todos').appendChild(p)
+       })
     }
-})
 
-const counterElement = document.createElement('h2')
-counterElement.textContent = `You have ${counter} tasks left:`
-h1Title = document.querySelector('h1')
-// document.querySelector("body").insertBefore(counterElement, firstPara)
-todoButton.insertAdjacentElement('afterend', counterElement)
-
-
+    renderToDos(todos, textFilter)
+        
 document.getElementById('to-do').addEventListener('click', (e) => {
     e.target.textContent = "Todo Added"
 })
 
-document.getElementById('new-todo').addEventListener("input", e => {
-    console.log(e.target.value);  
+document.getElementById('filter').addEventListener("input", e => {
+    textFilter.param = e.target.value
+    renderToDos(todos, textFilter)  
 })
